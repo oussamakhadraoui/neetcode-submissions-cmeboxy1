8fb836c40b55159053b25cf152e88f28ed0cd2af
oussama khadraoui <105125889+oussamakhadraoui@users.noreply.github.com>
2026-04-15 08:47:05 +0100

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-0.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int,int>in;
+    TreeNode* solve(vector<int>& preorder,int &index,int left,int right){
+        if(left>right)return NULL;
+        TreeNode* root=new TreeNode(preorder[index]);
+        int mid = in[preorder[index]];
+        index++;
+        root->left=solve(preorder,index,left,mid-1);
+        root->right=solve(preorder,index,mid+1,right);
+
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = inorder.size();
+        for(int i =0;i<n;i++){
+            in[inorder[i]]=i;
+        }
+        int index=0;
+        return solve(preorder,index,0,n-1);
+    }
+};
