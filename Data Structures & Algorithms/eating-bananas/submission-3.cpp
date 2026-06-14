7c02afd10a8da09b009c1bd887f1d1c40a63c25c
oussama khadraoui <105125889+oussamakhadraoui@users.noreply.github.com>
2026-06-14 02:26:26 +0100

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int i = 1 ;
+        int j = *max_element(piles.begin(),piles.end());
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            int count=0;
+            for(int &p:piles){
+                count+=p/mid;
+                if(p%mid)count++;
+            }
+            if(count<=h){
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }
+
+        }
+        return i;
+    }
+};
